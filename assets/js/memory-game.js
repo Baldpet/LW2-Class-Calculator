f1d37dc9c737e66d5631c5c1549ed2e2882e59df
Baldpet <peterdaniel.baldwin@gmail.com>
2020-06-30 17:38:54 +0000

    added card flip in javascript and deck declaration

---
+++ assets/js/memory-game.js
@@ -0,0 +1,10 @@
+const soldierDeck = ["soldier1","soldier2","soldier3", "soldier4", "soldier5", "soldier6", "soldier7"];
+const alienDeck = ["muton", "chryssalid", "sectoid"];
+
+// ---------card flipping ------------
+
+$(".game-card-style").click(function(){
+    $(this).toggleClass("flip");
+});
+
+
