e043625e34078085943e8dff0c5ca7531131a271
Baldpet <peterdaniel.baldwin@gmail.com>
2020-07-01 18:10:58 +0000

    added timer

--- assets/js/memory-game.js
+++ assets/js/memory-game.js
@@ -162,6 +162,16 @@ function newGame(){
 
 // ---------timer & best time ------------
 
+function startTimer() {
+     var sec = 0;
+    function pad ( val ) { return val > 9 ? val : "0" + val; }
+    setInterval( function(){
+        $("#seconds").html(pad(++sec%60));
+        $("#minutes").html(pad(parseInt(sec/60,10)));
+    }, 1000);
+
+}
+
 // ---------attempt counter ------------
 
 // ---------start game ------------
@@ -169,8 +179,8 @@ function newGame(){
 $(".start").click(function(){
     $(".game-start-container").addClass("hidden");
     $(".card-container").removeClass("hidden");
-    newGame()
-    var deck = cardDraw();
+    newGame();
+    startTimer();
     
 })
 
