ab58354a55c15e5ce8aeeccf490fd8ec4b4e70be
Baldpet <peterdaniel.baldwin@gmail.com>
2020-07-01 14:24:28 +0000

    added functions to select cards from the const decks in line with difficulty

--- assets/js/memory-game.js
+++ assets/js/memory-game.js
@@ -44,6 +44,104 @@ $(".fa-star").click(function(){
 
 // ---------Card Drawing and Deck Structure ------------
 
+function deckOne(){
+    var deckOne = [];
+        
+    var soldierDeckOne = shuffle(soldierDeck);
+    for(var i = 0; i < soldierDeckOne.length; i++) {
+        deckOne.push(soldierDeckOne[i]);
+        deckOne.push(soldierDeckOne[i]);
+    }
+
+    var alienDeckOne = shuffle(alienDeck);
+    for(var i = 0; i < alienDeckOne.length - 2; i++) {
+        deckOne.push(alienDeckOne[i]);
+        deckOne.push(alienDeckOne[i]);
+    }
+    
+    console.log(soldierDeckOne);
+    console.log(alienDeckOne);
+    console.log(deckOne);
+
+    shuffle(deckOne);
+
+    return deckOne;
+}
+
+function deckTwo(){
+    var deckTwo = [];
+        
+    var soldierDeckTwo = shuffle(soldierDeck);
+    for(var i = 0; i < soldierDeckTwo.length - 1; i++) {
+        deckTwo.push(soldierDeckTwo[i]);
+        deckTwo.push(soldierDeckTwo[i]);
+    }
+
+    var alienDeckTwo = shuffle(alienDeck);
+    for(var i = 0; i < alienDeckTwo.length - 1; i++) {
+        deckTwo.push(alienDeckTwo[i]);
+        deckTwo.push(alienDeckTwo[i]);
+    }
+    
+    console.log(soldierDeckTwo);
+    console.log(alienDeckTwo);
+    console.log(deckTwo);
+
+    shuffle(deckTwo);
+
+    return deckTwo;
+}
+
+function deckThree(){
+    var deckThree = [];
+        
+    var soldierDeckThree = shuffle(soldierDeck);
+    for(var i = 0; i < soldierDeckThree.length - 2; i++) {
+        deckThree.push(soldierDeckThree[i]);
+        deckThree.push(soldierDeckThree[i]);
+    }
+
+    var alienDeckThree = shuffle(alienDeck);
+    for(var i = 0; i < alienDeckThree.length; i++) {
+        deckThree.push(alienDeckThree[i]);
+        deckThree.push(alienDeckThree[i]);
+    }
+    
+    console.log(soldierDeckThree);
+    console.log(alienDeckThree);
+    console.log(deckThree);
+
+    shuffle(deckThree);
+
+    return deckThree;
+}
+
+function deckFour(){
+    var deckFour = [];
+        
+    var soldierDeckFour = shuffle(soldierDeck);
+    for(var i = 0; i < soldierDeckFour.length - 2; i++) {
+        deckFour.push(soldierDeckFour[i]);
+        deckFour.push(soldierDeckFour[i]);
+    }
+
+    var alienDeckFour = shuffle(alienDeck);
+    for(var i = 0; i < alienDeckFour.length - 1; i++) {
+        deckFour.push(alienDeckFour[i]);
+        deckFour.push(alienDeckFour[i]);
+        deckFour.push(alienDeckFour[i]);
+    }
+    
+    console.log(soldierDeckFour);
+    console.log(alienDeckFour);
+    console.log(deckFour);
+
+    shuffle(deckFour);
+
+    return deckFour;
+}
+
+
 function difficulty(){
     var difficultyNum = $(".star-active").length;
     return;
@@ -65,6 +163,10 @@ function cardDraw(){
 $(".start").click(function(){
     $(".game-start-container").addClass("hidden");
     $(".card-container").removeClass("hidden");
+    deckOne();
+    deckTwo();
+    deckThree();
+    deckFour();
 })
 
 // ---------card flipping ------------
