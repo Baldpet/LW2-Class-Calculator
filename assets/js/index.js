7426d8526376d0c80f46d11286a17a9d07279a93
Baldpet <peterdaniel.baldwin@gmail.com>
2020-07-10 14:16:47 +0000

    talent boxes now open their own description on mobile

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,21 +10,41 @@ function onYouTubeIframeAPIReady() {
 }
 
 // ----- Talents -------
-$(".talents-box").click(function(){
-    talentReveal($(this));
+$("#assault").click(function(){
+    talentClick();
 })
+
+
+function talentClick(){
+    $(".talents-box").click(function(){
+        talentReveal($(this));
+        removeTalent($(this));
+    })
+}
 
 
 function talentReveal(box){
     if (window.matchMedia('(max-width: 768px)').matches){
         var content = $(box).children("p").html();
         console.log(content)
-        if(content !== "Squaddie" || content !== "Lance Corporal" || content !== "Corporal" || content !== "Sergeant" || content !== "Staff Sergeant" || content !== "Tech Sergeant" || content !== "Gunnery Sergeant" || content !== "Master Sergeant"){
-           var boxNumber = $(box).index()
-           $(box).parent().next().children().eq(boxNumber).removeClass("hidden");
+        console.log(typeof(content))
+        if(content === "Squaddie" || content === "Lance Corporal" || content === "Corporal" || content === "Sergeant" || content === "Staff Sergeant" || content === "Tech Sergeant" || content === "Gunnery Sergeant" || content === "Master Sergeant"){
+            $(box).parent().next().children().addClass("hidden");
+        } else {
+            $(box).parent().next().children().addClass("hidden");
+            var boxNumber = $(box).index();
+            $(box).parent().next().children().eq(boxNumber).removeClass("hidden");
         }
-        //$(box)
     } else {
         $(box).parent().next().children().removeClass("hidden");
     }
 }
+
+function removeTalent(box){
+    $(box).off("click");
+    $(box).click(function(){
+        $(box).parent().next().children().addClass("hidden");
+        $(".talents-box").off("click");
+        talentClick();
+    })
+ }
