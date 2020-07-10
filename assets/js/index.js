6698edaf06a3f78ad646ac98fa8fb0f97d37d824
Baldpet <peterdaniel.baldwin@gmail.com>
2020-07-10 12:54:32 +0000

    Javascript working to unhide correct container on mobile and desktop

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+// ----- Youtube ---------
 var player;
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
@@ -6,4 +7,24 @@ function onYouTubeIframeAPIReady() {
       'onStateChange': onPlayerStateChange
     }
   });
+}
+
+// ----- Talents -------
+$(".talents-box").click(function(){
+    talentReveal($(this));
+})
+
+
+function talentReveal(box){
+    if (window.matchMedia('(max-width: 768px)').matches){
+        var content = $(box).children("p").html();
+        console.log(content)
+        if(content !== "Squaddie" || content !== "Lance Corporal" || content !== "Corporal" || content !== "Sergeant" || content !== "Staff Sergeant" || content !== "Tech Sergeant" || content !== "Gunnery Sergeant" || content !== "Master Sergeant"){
+           var boxNumber = $(box).index()
+           $(box).parent().next().children().eq(boxNumber).removeClass("hidden");
+        }
+        //$(box)
+    } else {
+        $(box).parent().next().children().removeClass("hidden");
+    }
 }
